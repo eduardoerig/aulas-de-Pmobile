cc161138deb387a2765955dfe5d52b108832ac90
Eduardo Felipe Storck Erig <137730731+eduardoerig@users.noreply.github.com>
2024-02-21 18:08:27 -0300

    Add files via upload

---
+++ Pasta curso/index.js
@@ -0,0 +1,10 @@
+/*
+let num = 1 //number
+let nome = 'Eduardo' //string
+print(num + nome);
+let verdade = true or false //bool
+let nulo= null //null
+let symble = symbol (1092) //symbol
+let indefinido = //undefined
+*/
+console.log(100 - 10); //imprimir no console
